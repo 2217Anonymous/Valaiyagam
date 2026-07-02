ae2233e077acb71242152d44dc6ae840a2850cb3
2217Anonymous <2217Anonymous@users.noreply.github.com>
2026-07-02 17:31:05 +0000

    deploy: 7bf8f3a2457c034199db97d322e4b3fb9966931c 7bf8f3a2457c034199db97d322e4b3fb9966931c

---
+++ _next/static/939JdvS3WzW9gxniYC5K6/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Valaiyagam//_next/:path+",
+        "destination": "/Valaiyagam/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
